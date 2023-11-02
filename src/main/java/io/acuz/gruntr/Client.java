11aed8285f8964dd2bf5382fa2c624eb93ea1168
VANDERMAST Kris <kris.vandermast@ext.ec.europa.eu>
2023-11-02 15:10:59 +0100

    initial commit

--- src/main/java/io/acuz/gruntr/Client.java
+++ src/main/java/io/acuz/gruntr/Client.java
@@ -2,18 +2,12 @@ package io.acuz.gruntr;
 
 import io.acuz.gruntr.vault.VaultRestClient;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.security.GeneralSecurityException;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Objects;
 import java.util.Properties;
-import java.util.regex.Pattern;
 
 public final class Client {
     private final Path path;
@@ -57,9 +51,9 @@ public final class Client {
                     var stringValue = ((String) val).trim();
 
                     if (stringValue.startsWith("ENC[") && stringValue.endsWith("]")) {
-                        var x = EncryptedProperty.of(stringValue);
+                        // var x = EncryptedProperty.of(stringValue);
 
-                        System.out.println("---->> " + x.decrypt(masterKey));
+                        //System.out.println("---->> " + x.decrypt(masterKey));
                     } else {
                         // non-encrypted value
                         properties.put(key, val);
@@ -102,58 +96,6 @@ public final class Client {
 
         private void validate() {
             Objects.requireNonNull(path);
-        }
-    }
-
-    private static final class EncryptedProperty {
-        private final byte[] data;
-        private final byte[] iv;
-        private final byte[] tag;
-        private final String type;
-
-        private EncryptedProperty(String data, String iv, String tag, String type) {
-            this.data = Base64.getDecoder().decode(data);
-            this.iv = Base64.getDecoder().decode(iv);
-            this.tag = Base64.getDecoder().decode(tag);
-            this.type = type;
-        }
-
-        static EncryptedProperty of(String value) {
-            var pattern  = Pattern.compile("ENC\\[AES256_GCM,data:(.+),iv:(.+),tag:(.+),type:(.+)\\]");
-            var matcher = pattern.matcher(value);
-
-            if(matcher.matches())  {
-                if(matcher.groupCount() == 4) {
-                    return new EncryptedProperty(
-                            matcher.group(1), // data
-                            matcher.group(2), // iv
-                            matcher.group(3), // tag
-                            matcher.group(4) // type
-                    );
-                }
-            }
-
-            throw new IllegalStateException("Value of encrypted property not correctly structured");
-        }
-
-        public String decrypt(byte[] secret) {
-            try {
-                var combinedData = new byte[data.length + this.tag.length];
-                System.arraycopy(this.data, 0, combinedData, 0, this.data.length);
-                System.arraycopy(this.tag, 0, combinedData, this.data.length, this.tag.length);
-
-                Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-                SecretKeySpec secretKeySpec = new SecretKeySpec(secret, "AES");
-                cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new GCMParameterSpec(128, iv));
-                cipher.updateAAD(new byte[]{});
-                var plaintext = cipher.doFinal(combinedData);
-
-                System.out.println(new String(plaintext));
-            } catch (GeneralSecurityException e) {
-                throw new RuntimeException(e);
-            }
-
-            return null;
         }
     }
 }
