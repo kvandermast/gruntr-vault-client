3351edd23ad01c80deb66cef542698aff611175c
VANDERMAST Kris <kris.vandermast@ext.ec.europa.eu>
2023-11-08 19:40:00 +0100

    Removed duplicate code, reusing client code for decryption

--- gruntr-vault-client-api/src/main/java/io/acuz/gruntr/vault/VaultTransitRestClientImpl.java
+++ gruntr-vault-client-api/src/main/java/io/acuz/gruntr/vault/VaultTransitRestClientImpl.java
@@ -25,8 +25,6 @@ import io.acuz.gruntr.vault.exception.VaultException;
 import io.acuz.gruntr.vault.model.VaultToken;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
