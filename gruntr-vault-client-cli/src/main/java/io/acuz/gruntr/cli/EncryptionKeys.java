9059e44469fd7d863cde72a9521b52b496ff1c06
VANDERMAST Kris <kris.vandermast@ext.ec.europa.eu>
2023-11-08 13:36:10 +0100

    Added tests for regexp matching for EncryptionKeys

--- gruntr-vault-client-cli/src/main/java/io/acuz/gruntr/cli/EncryptionKeys.java
+++ gruntr-vault-client-cli/src/main/java/io/acuz/gruntr/cli/EncryptionKeys.java
@@ -19,6 +19,7 @@ package io.acuz.gruntr.cli;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 final class EncryptionKeys {
     public static final String ALL = ".*";
@@ -29,20 +30,38 @@ final class EncryptionKeys {
         //no-op
     }
 
+    /**
+     * Register one or more new patterns. These will be used to evaluate if a key in the properties file requires
+     * encryption or not.
+     *
+     * @param pattern  the string representation of the RegExp to which the key must adhere
+     * @param patterns a vararg of string representing the RegExp to register
+     * @throws PatternSyntaxException – If the expression's syntax is invalid
+     */
     public static void register(String pattern, String... patterns) {
         PATTERNS.add(Pattern.compile(pattern).pattern());
 
-        if(null != patterns) {
+        if (null != patterns) {
             for (String p : patterns) {
                 PATTERNS.add(Pattern.compile(p).pattern());
             }
         }
     }
 
+    /**
+     * Clears the list of registered patterns
+     */
     static void clear() {
         PATTERNS.clear();
     }
 
+    /**
+     * Compiles the registered regexp into a single RegExp instance.
+     * For example, registering 'secret' and 'password' would generate a regexp '(secret|password)' which means that
+     * the key must either contain secret or password to match. The matching is performed case-insensitive.
+     *
+     * @return the compiled regexp.
+     */
     public static Pattern compile() {
         var buffer = new StringBuilder().append('(');
 
